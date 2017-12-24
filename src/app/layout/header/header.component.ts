38e778e3798bf4d9dc8b8eda34e4d94d12fecb48
wangxiang <1132735971@qq.com>
2017-12-24 16:30:05 +0800

    update github repository url

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -7,6 +7,8 @@ import { Router } from '@angular/router'
     styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit, OnDestroy {
+    githubRepositoryUrl = 'https://github.com/shawnwang5/todo-list'
+
     constructor(private router: Router) {
     }
 
